bb1184738dfa0e15aa59c2ec6a20798537296c94
Seungmin Jeong <sontneptm2@gmail.com>
2022-06-27 20:22:59 +0900

    update

---
+++ Softeer/micro_server.py
@@ -0,0 +1,11 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    N = int(sys.stdin.readline())
+    process = list(map(int,sys.stdin.readline().split()))
+
+   
+   
+print(process)
