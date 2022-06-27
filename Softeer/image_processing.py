20269d62ae57316ce374f8af77510f524cca643b
Seungmin Jeong <sontneptm2@gmail.com>
2022-06-27 19:32:54 +0900

    update

--- Softeer/image_processing.py
+++ Softeer/image_processing.py
@@ -1,46 +1,52 @@
 import sys
 from collections import deque
 
-H, W = map(int,sys.stdin.readline().split())
+H, W = map(int, sys.stdin.readline().split())
 
-image = list(map(int,sys.stdin.readline().split()))
-visited = [[False for _ in range(W)] for _ in range(H)]
-stack = deque()
+image = []
+
+for _ in range(H):
+    image.append(list(map(int, sys.stdin.readline().split())))
+
+Q = int(sys.stdin.readline())
 
 dx = [-1,1,0,0]
 dy = [0, 0,-1,1]
 
 def q_function(start_point, target):
+    visited = [[False for _ in range(W)] for _ in range(H)]
+    stack = deque()
     visited[start_point[0]-1][start_point[1]-1] = True
+    start_point = [start_point[0]-1,start_point[1]-1]
     stack.append(start_point)
 
     while stack:
         cursor = stack.pop()
-        x = cursor[0]-1
-        y = cursor[1]-1
+        x = cursor[0]
+        y = cursor[1]
 
         for i in range(4):
             new_dest = [x + dx[i] , y + dy[i]]
 
-            if new_dest[0] <= 0 or new_dest[0] >= H: continue
-            if new_dest[1] <= 0 or new_dest[1] >= W: continue
+
+            if new_dest[0] < 0 or new_dest[0] >= H: 
+                continue
+            if new_dest[1] < 0 or new_dest[1] >= W: 
+                continue
         
-            if image[new_dest[0]][new_dest[1]] == image[x][y]:
+            if image[new_dest[0]][new_dest[1]] == image[x][y] and not visited[new_dest[0]][new_dest[1]]:
                 visited[new_dest[0]][new_dest[1]] = True
                 stack.append(new_dest)
 
     for i in range(H):
         for j in range(W):
             if visited[i][j]: 
-                print("i",type(target))
-                print("i",j)
-                print("img", image)
                 image[i][j] = target
 
-Q = int(sys.stdin.readline())
-
 for _ in range(Q):
-    x, y, t = list(map(int,sys.stdin.readline().split()))
+    x, y, t = list(map(int,input().split()))
     q_function([x,y], t)
 
-print(image)
+for i in range(len(image)):
+    print(*image[i])
+
