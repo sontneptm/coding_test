2faac444b227f0f5a64fc90497811b6fb6ccebb9
Seungmin Jeong <sontneptm2@gmail.com>
2022-06-29 18:41:31 +0900

    update

---
+++ greedy/gas_station.py
@@ -0,0 +1,20 @@
+N = int(input())
+distance_list = list(map(int, input().split()))
+value_list = list(map(int, input().split()))
+
+min_val = min(value_list)
+
+total = 0
+prev_val = value_list[0]
+prev_idx = 0
+
+for i in range(N):  
+    if prev_val == min_val:
+        total += prev_val * (sum(distance_list[prev_idx:N]))
+        break
+    elif value_list[i] < prev_val:
+        total += prev_val * (sum(distance_list[prev_idx:i]))
+        prev_val = value_list[i]
+        prev_idx = i
+
+print(total)
