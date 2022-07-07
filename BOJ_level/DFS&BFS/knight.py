ba7fc3b605eaebc934a28d0c5864a82fa1899174
Seungmin Jeong <sontneptm2@gmail.com>
2022-07-07 18:42:31 +0900

    update

--- BOJ_level/DFS&BFS/knight.py
+++ BOJ_level/DFS&BFS/knight.py
@@ -6,7 +6,6 @@ from collections import deque
 N = int(input())
 
 for _ in range(N):
-    move = 0
     I = int(input())
     table = [[0 for _ in range(I)] for _ in range(I)]
     knight_stack = deque()
@@ -14,25 +13,24 @@ for _ in range(N):
     target_loc = list(map(int, input().split()))
 
     knight_stack.append(start_loc)
-    table[start_loc[0]][start_loc[1]] = 0
 
     dx = [-2, -1, 1, 2, 2, 1, -1, -2]
     dy = [-1, -2, -2, -1, 1, 2, 2, 1]
 
-    while knight_stack:
-        y, x = knight_stack.popleft()
-        if [y,x] == target_loc : break
+    def bfs():
+        while knight_stack:
+            y, x = knight_stack.popleft()
+            if [y,x] == target_loc : return 0
 
-        for i in range(8):
-            if (0 <= y + dy[i] < I) and (0 <= x + dx[i] < I):
-                ny = y + dy[i]
-                nx = x + dx[i]
+            for i in range(8):
+                if (0 <= y + dy[i] < I) and (0 <= x + dx[i] < I):
+                    ny = y + dy[i]
+                    nx = x + dx[i]
 
-                if table[ny][nx] == 0:
-                    table[ny][nx] = table[y][x] + 1
-                    knight_stack.append([ny, nx])
+                    if table[ny][nx] == 0:
+                        table[ny][nx] = table[y][x] + 1
+                        if [ny, nx] == target_loc:
+                            return table[ny][nx]
+                        knight_stack.append([ny, nx])
 
-    for t in table:
-        move = max(*t, move)
-
-    print(move)
+    print(bfs())
