f9a51581ba9959178fd93ebf4860ea574572cf9e
Seungmin Jeong <sontneptm2@gmail.com>
2022-07-17 18:37:15 +0900

    update

--- BOJ_level/DFS&BFS/hide&seek.py
+++ BOJ_level/DFS&BFS/hide&seek.py
@@ -1,3 +1,25 @@
 # BOJ 1697
 # 숨바꼭질
 
+from calendar import c
+from collections import deque
+
+N, K = map(int,input().split())
+visited = [0 for _ in range(100001)]
+
+queue = deque()
+queue.append(N)
+
+def bfs():
+    while queue:
+        cursor = queue.popleft()
+        if cursor == K : return visited[cursor]
+
+        for nd in (cursor-1, cursor+1, 2*cursor):
+            if (0 <= nd <= 100000) and (visited[nd] == 0) :
+                visited[nd] = visited[cursor] + 1
+                queue.append(nd)
+
+                if nd == K: return visited[nd]
+            
+print(bfs())
