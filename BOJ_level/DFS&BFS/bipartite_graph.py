41c1480899f5fe48b3e58c36fe036fbf5932ec11
Seungmin Jeong <sontneptm2@gmail.com>
2022-07-19 09:37:59 +0900

    update

--- BOJ_level/DFS&BFS/bipartite_graph.py
+++ BOJ_level/DFS&BFS/bipartite_graph.py
@@ -3,28 +3,40 @@
 
 from collections import deque
 
+def bfs():
+    while queue:
+        cursor = queue.popleft()
+
+        for g in graph[cursor]:
+            if visited[g] == 0:
+                visited[g] = visited[cursor] * -1
+                queue.append(g)
+            elif visited[g] == visited[cursor]:
+                return False
+    return True
+    
 K = int(input())
 
-V, E = map(int, input().split())
+for _ in range(K):
+    V, E = map(int, input().split())
 
-graph = [[] for _ in range(V+1)]
-visited = [0 for _ in range(V+1)]
-stack = deque()
+    graph = [[] for _ in range(V+1)]
+    visited = [0 for _ in range(V+1)]
+    queue = deque()
+    is_bipartite = True
 
-for _ in range(E):
-    start, end = map(int, input().split())
+    for _ in range(E):
+        start, end = map(int, input().split())
 
-    graph[start].append(end)
-    graph[end].append(start)
+        graph[start].append(end)
+        graph[end].append(start)
 
-stack.append(1)
-visited[1] = 1
+    for i in range(1, V+1):
+        if visited[i] == 0:
+            queue.append(i)
+            visited[i] = 1
+            is_bipartite = bfs()
 
-while stack:
-    cursor = stack.pop() 
-    
-    for d in graph[cursor]:
-        
+        if not is_bipartite: break
 
-
-
+    print("YES" if is_bipartite else "NO")
